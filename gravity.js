af0419cf3991c14c50efe1a06e0e3d18ec8be891
andresbr92 <andresbr92@gmail.com>
2020-12-17 14:00:18 +0100

    espiral

---
+++ gravity.js
@@ -0,0 +1,96 @@
+function gravity() {
+  let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Svg = Matter.Svg;
+
+  // create engine
+  let engine = Engine.create(),
+    world = engine.world;
+
+  //lenght and with
+  const w = window.visualViewport.width;
+  const h = window.visualViewport.height;
+
+  // create renderer
+  const canvasSnow = document.getElementById("canvasSnow");
+  let render = Render.create({
+    element: canvasSnow,
+    engine: engine,
+    options: {
+      width: w,
+      height: h,
+      wireframes: false,
+      background: "#ffffff00",
+    },
+  });
+
+  Render.run(render);
+
+  // create runner
+  let runner = Runner.create();
+  Runner.run(runner, engine);
+
+  let gravity = engine.world.gravity;
+
+  // snowBall
+  const sbRadius = w / 2.3;
+  const mfRadius = sbRadius / 30;
+
+  let ballRadius = w / 2.2;
+
+  World.add(engine.world, [Bodies.circle(w/2, h/2, sbRadius, {isStatic:true, render: {strokeStyle: "#fff", visible: 0}})]);
+ 
+
+  setInterval(() => {
+    gravity.x = Common.random(-0.1, 0.1);
+    gravity.y = 0.3;
+
+    World.add(world, [
+      Bodies.circle(
+        Common.random(20, window.visualViewport.width),
+        -10,
+        Common.random(mfRadius, mfRadius / 1.5),
+        { render: { fillStyle: "#fff", strokeStyle: "#fff" } }
+      ),
+
+      Bodies.circle(
+        Common.random(20, window.visualViewport.width),
+        -10,
+        Common.random(mfRadius, mfRadius / 1.5),
+        { render: { fillStyle: "#fff", strokeStyle: "#fff" } }
+      ),
+    ]);
+
+    const allBodies = Matter.Composite.allBodies(world);
+    allBodies.forEach((element) => {
+      if (
+        (element.position.x > window.visualViewport.width + 50) |
+        (element.position.x < 0)
+      ) {
+        Matter.Composite.remove(world, element);
+      }
+    });
+  }, 400);
+
+  World.add(world, [
+    Bodies.rectangle(0, h, w * 2, 20,
+      { isStatic: true }
+    )
+  ]);
+
+  // add mouse control
+ 
+
+  // fit the render viewport to the scene
+  Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: window.visualViewport.width, y: window.visualViewport.height },
+  });
+}
---
+++ snowball.js
@@ -0,0 +1,208 @@
+function snowBall() {
+  let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    World = Matter.World,
+    Common = Matter.Common,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body;
+
+  // create engine
+  let engine = Engine.create(),
+    world = engine.world;
+
+  //lenght and with
+  const wBall = window.visualViewport.width;
+  const hBall = window.visualViewport.height;
+
+  // create renderer
+  const canvasBall = document.getElementById("canvasBall");
+  let render = Render.create({
+    element: canvasBall,
+    engine: engine,
+    options: {
+      width: wBall,
+      height: hBall,
+      wireframes: false,
+      background: "#ffffff00",
+    },
+  });
+
+  Render.run(render);
+
+  // create runner
+  let runner = Runner.create();
+  Runner.run(runner, engine);
+  //variables
+  const snowFlakesArray = [];
+  let gravity = world.gravity;
+  let innerSmallBallRadius = wBall / 25;
+
+
+  // snowBall
+  const sbRadius = wBall / 3;
+
+  // miniflakes
+  let mfArray = [];
+  const mfRadius = sbRadius / 30;
+  
+  let numBalls = 400
+  for (let i = 0; i < numBalls; i++) {
+    let xPos = wBall / 2 + (innerSmallBallRadius + (sbRadius - innerSmallBallRadius) * i / numBalls ) * Math.cos(i * 2 * Math.PI / numBalls)
+    let yPos = hBall / 2 + (innerSmallBallRadius + (sbRadius - innerSmallBallRadius) * i / numBalls ) * Math.sin(i * 2 * Math.PI / numBalls)
+    console.log(xPos, yPos)
+    let mfBall = Bodies.circle(
+      xPos,
+      yPos,
+      Common.random(mfRadius, mfRadius / 1.3),
+      { render: { fillStyle: "#fff", strokeStyle: "#fff" } }
+    );
+
+    mfArray.push(mfBall);
+  }
+  World.add(world, mfArray);
+  let innerBallRadius = wBall / 7.4;
+  World.add(
+    world,
+    Bodies.circle(wBall / 2, hBall / 2, innerBallRadius, {
+      isStatic: true,
+      render: {
+        visible: 0,
+      },
+    })
+  );
+
+  
+  World.add(
+    world,
+    Bodies.circle(wBall / 2, 63 * hBall / 100, innerSmallBallRadius, {
+      isStatic: true,
+      render: {
+        visible: 0,
+      },
+    })
+  );
+
+  let ballRadius = wBall / 2.3;
+  let subDivisions = 90;
+
+  for (let i = 0; i < subDivisions; i++) {
+    a = Bodies.rectangle(
+      wBall / 2 + ballRadius * Math.cos((i * 4 * Math.PI) / 180),
+      hBall / 2 + ballRadius * Math.sin((i * 4 * Math.PI) / 180),
+      16 * ballRadius / subDivisions,
+      16 * ballRadius / subDivisions,
+      {
+        isStatic: true,
+        angle: (Math.PI / 180) * i * 4,
+        render: {
+          fillStyle: "#000",
+          strokeStyle: "#fff",
+          lineWidth: 0,
+          visible: 0,
+        },
+      }
+    );
+    World.add(world, a);
+  }
+
+  setInterval(() => {
+    //shake finished rest
+
+    if (!shakeFinished && alphaValue > 0.1 ){
+      alphaValue -= 0.05;
+	  document.getElementById('fabsText').innerHTML = '¡Agita!'
+      document.getElementById('fabsRobotics').style.color = "rgba(53,53,53,"+alphaValue+")";
+    }
+
+    //apply forces to body elements
+
+    valGravityX != undefined
+      ? (gravity.x = (valGravityX * -1) / 100)
+      : (gravity.x = 0);
+    valGravityY != undefined
+      ? (gravity.y = valGravityY / 100)
+      : (gravity.y = 0.05);
+
+	  if(valForceX < -4 || valForceX > 4 || valForceY < -4 || valForceY > 4) setAlphaValue();
+    mfArray.forEach((ball) => {
+      if (valForceX > 4) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: 0.002, y: 0 }
+        );
+        return;
+      } else if (valForceX < -4) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: -0.002, y: 0 }
+        );
+        return;
+      } else if (valForceY > 4) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: 0, y: 0.002 }
+        );
+        return;
+      } else if (valForceY < -4) {
+        Body.applyForce(
+          ball,
+          { x: ball.position.x, y: ball.position.y },
+          { x: 0, y: -0.002 }
+        );
+        return;
+      }
+    });
+    //delete elements out of window
+    const allBodies = Matter.Composite.allBodies(world);
+    allBodies.forEach((element) => {
+      if (
+        (element.position.x > window.visualViewport.width + 50) ||
+        (element.position.x < 0 || element.position.y > hBall)
+      ) {
+        Matter.Composite.remove(world, element);
+        mfArray.splice(element,1)
+
+
+      }
+    });
+    //add boddies deleted again inside ball
+	if (mfArray.length < 200) {
+		
+		console.log('estaoy poerdiendo bolas')
+		for(let j = 0; j < (200-mfArray.length); j++){
+			let mfBall = Bodies.circle(
+				wBall / 2,
+				hBall / 2,
+				Common.random(mfRadius, mfRadius / 1.3),
+				{ render: { fillStyle: "#fff", strokeStyle: "#fff" } }
+			);
+			mfArray.push(mfBall);
+			World.add(world,mfBall);
+		}
+		//World.add(world,mfArray);
+	}
+	console.log(mfArray.length)
+  }, 200);
+
+  // fit the render viewport to the scene
+  Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: window.visualViewport.width, y: window.visualViewport.height },
+  });
+}
+
+function setAlphaValue(){
+	let d = new Date;
+	console.log(d.getTime()+", alphaValue: "+alphaValue);
+	alphaValue += 0.2;
+    if(alphaValue >= 1.0){
+  	shakeFinished = true;
+  	alphaValue = 1.0;
+    }
+	shakeFinished ? document.getElementById('fabsText').innerHTML = 'Fabs Robotics <br> os desea <br> felices fiestas' : null
+    document.getElementById('fabsRobotics').style.color = "rgba(53,53,53,"+alphaValue+")";
+}
